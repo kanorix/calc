72e65ba2d79127c7641cd346811e7958e5b8550a
kanorix <kanorix320@gmail.com>
2022-07-19 08:56:41 +0900

    無駄な差分の削除

--- android/app/src/main/kotlin/net/kanorix/calc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package net.kanorix.calc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/providers/caluculate_provider.dart
+++
@@ -1,7 +0,0 @@
-
-// final binaryOperatorMap = {
-//   InputButton.plus: (num a, num b) => (a + b),
-//   InputButton.minus: (num a, num b) => (a - b),
-//   InputButton.multi: (num a, num b) => (a * b),
-//   InputButton.div: (num a, num b) => (a / b),
-// };
